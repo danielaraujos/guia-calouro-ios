31391a7433b6d11d9dbb91ea65139e229ccedd7f
Daniel Araujo <danielaraujos@live.com>
2017-02-08 08:42:10 -0200

    Upload do projeto

---
+++ Guia do Calouro/HomeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Guia do Calouro
+//
+//  Created by Daniel Araújo on 08/02/17.
+//  Copyright © 2017 Daniel Araújo Silva. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.addSlideMenuButton()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-Guia do Calouro/Pods-Guia do Calouro-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Guia_do_Calouro : NSObject
+@end
+@implementation PodsDummy_Pods_Guia_do_Calouro
+@end
